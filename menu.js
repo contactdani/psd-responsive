b58e9f1180e41b1c4e1e9f8bd7136159232777c2
Zargar Danish <danishzargar99@gmail.com>
2017-03-16 14:19:18 +0530

    Minor Bugs Fixed
    Website template now more Responsive...

--- menu.js
+++ menu.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $("#burger-nav").on("click", function(){
-    $("header nav ul").toggleClass("open");
+    $("header nav ul").toggleClass("open animateburger");
   });
 
 });
